c4d3b013744d985fc9d572c9ae93c825bdd59419
iloominate <aliakseiklimau@gmail.com>
2022-04-18 16:26:48 +0200

    htab_resize modified

--- htab_resize.c
+++ htab_resize.c
@@ -1,3 +1,15 @@
 //
 // Created by aliakseiklimau on 4/18/22.
 //
+
+#include "htab_pr.h"
+#include "htab.h"
+
+void htab_resize(htab_t *table, size_t newn)
+{
+    if (newn <= table->arr_size)
+        return;
+
+
+
+}
