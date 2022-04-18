49aa0fae36313b6a60d4befcd8f7aa4d8825993a
iloominate <aliakseiklimau@gmail.com>
2022-04-18 15:34:37 +0200

    htab_pr is done

--- htab_pr.h
+++ htab_pr.h
@@ -5,4 +5,21 @@
 #ifndef UNTITLED2_HTAB_PR_H
 #define UNTITLED2_HTAB_PR_H
 
+
+#include "htab.h"
+
+struct htab_item
+{
+    htab_pair_t pair;
+    struct htab_item *next;
+
+};
+
+struct htab
+{
+    size_t size;
+    size_t arr_size;
+    size_t *arr_ptr[];
+};
+
 #endif //UNTITLED2_HTAB_PR_H
