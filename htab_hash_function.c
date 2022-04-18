820f14c55dfa791bbdc91bea9be7f813bd6e3d74
iloominate <aliakseiklimau@gmail.com>
2022-04-18 16:31:12 +0200

    htab_hash_function done

--- htab_hash_function.c
+++ htab_hash_function.c
@@ -1,3 +1,15 @@
 //
 // Created by aliakseiklimau on 4/18/22.
 //
+
+#include "htab.h"
+#include "htab_pr.h"
+#include <stdint.h>
+
+size_t htab_hash_function(const char *str) {
+    uint32_t h=0;     // musí mít 32 bitů
+    const unsigned char *p;
+    for(p=(const unsigned char*)str; *p!='\0'; p++)
+        h = 65599*h + *p;
+    return h;
+}
