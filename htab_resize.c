4a90d730e0a19a7ee098c0e1cb6ebed1fc12538e
iloominate <aliakseiklimau@gmail.com>
2022-04-19 17:10:55 +0200

    resize modified v1

--- htab_resize.c
+++ htab_resize.c
@@ -7,8 +7,27 @@
 
 void htab_resize(htab_t *table, size_t newn)
 {
+    htab_t *new_table;
+    htab_t *tmp_table = table;
 
+    new_table = htab_init(newn);
 
+    for (int i = 0; i < table->arr_size; i++)
+    {
+        struct htab_item *tmp_item = table->arr_ptr[i];
 
+        while (tmp_item->next != NULL)
+        {
+            htab_lookup_add(new_table, tmp_item->pair.key);
+            tmp_item = tmp_item->next;
+        }
+        // check the last bucket
+        if (tmp_item->pair.key != NULL)
+            htab_lookup_add(new_table, tmp_item->pair.key);
+    }
+
+    table = new_table;
+    htab_clear(tmp_table);
+    htab_free(tmp_table);
 
 }
