dc8c057f45ae8fdfea408529dee57973ba88941d
iloominate <aliakseiklimau@gmail.com>
2022-04-19 16:29:08 +0200

    htab_free is done

--- htab_free.c
+++ htab_free.c
@@ -1,3 +1,15 @@
 //
 // Created by aliakseiklimau on 4/19/22.
 //
+
+
+#include "htab_pr.h"
+#include "htab.h"
+#include <malloc.h>
+
+
+void htab_free(htab_t * table)
+{
+    htab_clear(table);
+    free(table);
+}
