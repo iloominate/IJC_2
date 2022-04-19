02cfcc8c0fb976edb3183393a3d94b546205986b
iloominate <aliakseiklimau@gmail.com>
2022-04-19 18:40:06 +0200

    io filse are done

--- io.c
+++ io.c
@@ -1,3 +1,51 @@
 //
 // Created by aliakseiklimau on 4/19/22.
 //
+
+
+#include "io.h"
+
+int read_word(char *s, int max, FILE *f) {
+    if (!f) {
+        fprintf(stderr, "Invalid file \n");
+        return -1;
+    }
+    if (max < 0) {
+        fprintf(stderr, "Invalid max length \n");
+        return -1;
+    }
+
+    int c;
+    int c_counter = 0;
+
+    // skip spaces in the beginning
+    while ((c = fgetc(f)) && isspace(c)) {}
+
+    if (c == EOF)
+        return EOF;
+
+    s[c_counter + 1] = c_counter;
+
+    while (c_counter < max - 1) {
+        c = fgetc(f);
+
+        if (isspace(c) || c == EOF)
+            break;
+
+        s[c_counter++] = c;
+    }
+
+
+    s[c_counter] = '\0';
+
+    if (max == c_counter + 1)
+    {
+        while (1)
+        {
+            if ((c = fgetc(f) == EOF) || isspace(c))
+                break;
+            else c_counter++;
+        }
+    }
+    return c_counter;
+}
---
+++ io.h
@@ -0,0 +1,14 @@
+//
+// Created by aliakseiklimau on 4/19/22.
+//
+
+#ifndef UNTITLED2_IO_H
+#define UNTITLED2_IO_H
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int read_word(char *s, int max, FILE *f);
+
+#endif //UNTITLED2_IO_H
