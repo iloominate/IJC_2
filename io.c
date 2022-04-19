d6456fe1ac2b48d351a2f15c698d3e09a7215c74
iloominate <aliakseiklimau@gmail.com>
2022-04-19 18:09:14 +0200

    wordcount added and modified

---
+++ io.c
@@ -0,0 +1,3 @@
+//
+// Created by aliakseiklimau on 4/19/22.
+//
--- wordcount.c
+++ wordcount.c
@@ -1,3 +1,33 @@
 //
 // Created by aliakseiklimau on 4/16/22.
 //
+
+#include "htab.h"
+#include "htab_pr.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include <stdint.h>
+
+#ifdef HASHTEST
+
+// djb2 by Dan Bernstein
+size_t htab_hash_function (const char *str)
+{
+    uint32_t hash = 5381;
+    int c;
+    while (c = *str++)
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    return hash;
+}
+#endif
+// min length 100 + null byte
+#define MIN_SIZE_WRD 101
+
+// Prime number for max length
+#define MAX_SIZE_WRD 16651
+
+int main (void)
+{
+
+
+}
